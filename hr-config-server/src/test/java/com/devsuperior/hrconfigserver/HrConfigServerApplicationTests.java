b510e640599c8eea71897f35d9756e130bdc9c6e
evandro <evandrolt@gmail.com>
2021-12-19 16:01:52 -0300

    Private Git repository

--- hr-config-server/src/test/java/com/devsuperior/hrconfigserver/HrConfigServerApplicationTests.java
+++ hr-config-server/src/test/java/com/devsuperior/hrconfigserver/HrConfigServerApplicationTests.java
@@ -3,9 +3,10 @@ package com.devsuperior.hrconfigserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class HrConfigServerApplicationTests {
-
+	
 	@Test
 	void contextLoads() {
 	}
