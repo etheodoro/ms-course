f939691f66bd281eaea56660840f8f80716daa90
evandro <evandrolt@gmail.com>
2022-01-02 23:19:06 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.feignClients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	private static Logger log = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private  UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		
+		if (user == null) {
+			log.error("Emial not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		log.info("Email found: " + email);
+		return user;
+	}
+	
+}
