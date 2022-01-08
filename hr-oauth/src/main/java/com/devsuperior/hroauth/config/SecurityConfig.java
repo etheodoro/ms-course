3cddd4f3547bb789c48dff99057774d698f6cf1a
evandro <evandrolt@gmail.com>
2022-01-08 18:36:01 -0300

    Cors Configuration

--- hr-oauth/src/main/java/com/devsuperior/hroauth/config/SecurityConfig.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.devsuperior.hroauth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService userDetailsService;
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.authorizeRequests().antMatchers(HttpMethod.GET, "/users/search/**").permitAll();
+	}
+	
 	@Override
 	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
